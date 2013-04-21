f65820ee40c5edd84bbbee9e7e4cc8b96013d701
Cheeseen <aychtang@gmail.com>
2013-04-21 18:39:01 +0100

    filestructure

--- client/index.js
+++ client/index.js
@@ -1,8 +1,8 @@
 (function(){
 	var socket = io.connect();
 	var context = document.getElementById('canvas').getContext('2d');
-	context.canvas.width = 1500;
-	context.canvas.height = 1500;
+	context.canvas.width = 600;
+	context.canvas.height = 600;
 
 	$('canvas').mousemove(function(e){
 		if(e.offsetX) {
@@ -13,7 +13,7 @@
 	});
 
 	socket.on('newPos', function(data) {
-		context.clearRect(0,0,1500,1500);
+		context.clearRect(0,0,600,600);
 		for (var player in data) {
 			context.fillRect(data[player].x, data[player].y, 15, 15);
 		}
