347ac9eac9eb76203da0e42e6babd1cc00d94bd4
Cheeseen <aychtang@gmail.com>
2013-04-21 19:15:36 +0100

    adds names to square

--- client/index.js
+++ client/index.js
@@ -1,6 +1,12 @@
 (function(){
 	var socket = io.connect();
 	var context = document.getElementById('canvas').getContext('2d');
+	var name = prompt('enter your name bitch');
+
+	if (name) {
+		socket.emit('playerName', name);
+	}
+
 	context.canvas.width = 600;
 	context.canvas.height = 600;
 
@@ -16,6 +22,7 @@
 		context.clearRect(0,0,600,600);
 		for (var player in data) {
 			context.fillRect(data[player].x, data[player].y, 15, 15);
+			context.fillText(data[player].name, data[player].x, data[player].y + 25);
 		}
 	});
 }())
--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ var players = {};
 var player = function(socket) {
 	return {
 		id : socket.id,
+    name: null,
 		x : null,
 		y : null
 	};
@@ -27,10 +28,15 @@ var player = function(socket) {
 
 io.sockets.on('connection', function (socket) {
 	players[socket.id] = player(socket);
+
   socket.on('updatePosition', function (data) {
   	players[socket.id].x = data.x;
   	players[socket.id].y = data.y;
   	io.sockets.emit('newPos', players);
+  });
+
+  socket.on('playerName', function(data) {
+    players[socket.id].name = data;
   });
 
   socket.on('disconnect', function() {
